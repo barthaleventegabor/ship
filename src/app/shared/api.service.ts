b26c7111480f2f78562cab9c19a6935fc8df84fa
Bartha Levente <b.levi.levi@gmail.com>
2025-10-18 11:57:46 +0200

    Kezdés, beüzemelés, illetve service-ban getShipments() megírva

---
+++ src/app/shared/api.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  host: string = "http://localhost:8000/api/"
+
+  constructor(private http : HttpClient) { }
+
+  getShipments(){
+    const url = this.host + "shipments"
+    return this.http.get(url)
+  }
+}
